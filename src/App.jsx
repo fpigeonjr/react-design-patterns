2b1cd9ba579a6163abb506b40d32cf125ca2a630
fpigeonjr <frank.pigeonjr@gmail.com>
2023-03-05 21:44:07 -0600

    adds lists for people

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { SplitScreen } from "./SplitScreen"
 import { people, products } from "./data"
 import { RegularList } from "./RegularList"
 import { SmallPersonListItem } from "./people/SmallPersonListItem"
+import { LargePersonListItem } from "./people/LargePersonListItem"
 
 const LeftHandComp = ({ name }) => <h1 style={{ backgroundColor: "green" }}>Hello {name}</h1>
 const RightHandComp = ({ message, children }) => {
@@ -26,6 +27,11 @@ function App() {
           items={people}
           resourceName="person"
           itemComponent={SmallPersonListItem}
+        />
+        <RegularList
+          items={people}
+          resourceName="person"
+          itemComponent={LargePersonListItem}
         />
       </RightHandComp>
     </SplitScreen>
