94c6ccecab284a83361cf6d721d64d2a0c68ddb5
fpigeonjr <frank.pigeonjr@gmail.com>
2023-03-08 17:24:56 -0600

    localStorage  example

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import "./App.css"
+import axios from "axios"
 import { CurrentUserLoader } from "./CurrentUserLoader"
 import { UserLoader } from "./UserLoader"
 import { ResourceLoader } from "./ResourceLoader"
 import { ProductInfo } from "./ProductInfo"
 import { DataSource } from "./DataSource"
 import { UserInfo } from "./UserInfo"
-import axios from "axios"
 
 const getServerData = (url) => async () => {
   const response = await axios.get(url)
   return response.data
 }
+
+const getLocalStorageData = (key) => () => {
+  return localStorage.getItem(key)
+}
+
+const Text = ({ message }) => <h1>{message}</h1>
 
 function App() {
   return (
@@ -20,6 +26,12 @@ function App() {
         resourceName="user"
       >
         <UserInfo />
+      </DataSource>
+      <DataSource
+        getDataFunc={getLocalStorageData("message")}
+        resourceName="message"
+      >
+        <Text />
       </DataSource>
     </>
   )
