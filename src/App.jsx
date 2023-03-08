a23e7db5bbd3141fa782949087f6dc1bfc8c0983
fpigeonjr <frank.pigeonjr@gmail.com>
2023-03-08 16:04:18 -0600

    adds  UserLoader to allow for user  lookup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css"
 import { CurrentUserLoader } from "./CurrentUserLoader"
+import { UserLoader } from "./UserLoader"
 import { UserInfo } from "./UserInfo"
 
 function App() {
   return (
-    <CurrentUserLoader>
-      <UserInfo />
-    </CurrentUserLoader>
+    <>
+      <UserLoader userId="123">
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId="234">
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId="345">
+        <UserInfo />
+      </UserLoader>
+    </>
   )
 }
 
