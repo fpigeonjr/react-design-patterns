2ced20f7da4406b3da20adf89f4ad1c5270401d8
fpigeonjr <frank.pigeonjr@gmail.com>
2023-03-08 17:46:16 -0600

    adds uncontrolled form

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,11 @@
 import "./App.css"
-import axios from "axios"
-import { CurrentUserLoader } from "./CurrentUserLoader"
-import { UserLoader } from "./UserLoader"
-import { ResourceLoader } from "./ResourceLoader"
-import { ProductInfo } from "./ProductInfo"
-import { DataSource } from "./DataSource"
-import { UserInfo } from "./UserInfo"
-
-const getServerData = (url) => async () => {
-  const response = await axios.get(url)
-  return response.data
-}
-
-const getLocalStorageData = (key) => () => {
-  return localStorage.getItem(key)
-}
-
-const Text = ({ message }) => <h1>{message}</h1>
+import { UncontrolledForm } from "./UncontrolledForm"
 
 function App() {
   return (
     <>
-      <DataSource
-        getDataFunc={getServerData("/api/users/123")}
-        resourceName="user"
-      >
-        <UserInfo />
-      </DataSource>
-      <DataSource
-        getDataFunc={getLocalStorageData("message")}
-        resourceName="message"
-      >
-        <Text />
-      </DataSource>
+      <h1>UnControlled Comps</h1>
+      <UncontrolledForm />
     </>
   )
 }
