12427f4b58204174082d9b888458710de10e020b
fpigeonjr <frank.pigeonjr@gmail.com>
2023-03-08 19:16:19 -0600

    adds finish onBoarding

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,30 @@ import { UncontrolledOnboardingFlow } from "./UncontrolledOnboardingFlow"
 const StepOne = ({ goToNext }) => (
   <>
     <h1>Step 1</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ name: "John Doe" })}>Next</button>
   </>
 )
 const StepTwo = ({ goToNext }) => (
   <>
     <h1>Step 2</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ age: 100 })}>Next</button>
   </>
 )
 const StepThree = ({ goToNext }) => (
   <>
     <h1>Step 3</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ hairColor: "brown" })}>Next</button>
   </>
 )
 
 function App() {
   return (
-    <UncontrolledOnboardingFlow>
+    <UncontrolledOnboardingFlow
+      onFinish={(data) => {
+        console.log(data)
+        alert("Finished Onboarding")
+      }}
+    >
       <StepOne />
       <StepTwo />
       <StepThree />
--- src/UncontrolledOnboardingFlow.jsx
+++ src/UncontrolledOnboardingFlow.jsx
@@ -2,7 +2,23 @@ import React from "react"
 export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
   const [onBoardingData, setOnBoardingData] = React.useState({})
   const [currentIndex, setCurrentIndex] = React.useState(0)
-  const goToNext = () => setCurrentIndex(currentIndex + 1)
+
+  const goToNext = (stepData) => {
+    const nextIndex = currentIndex + 1
+    const updatedData = {
+      ...onBoardingData,
+      ...stepData
+    }
+    console.log(updatedData)
+
+    if (nextIndex < children.length) {
+      setCurrentIndex(nextIndex)
+    } else {
+      onFinish(updatedData)
+    }
+    setOnBoardingData(updatedData)
+  }
+
   const currentChild = React.Children.toArray(children)[currentIndex]
 
   if (React.isValidElement(currentChild)) {
